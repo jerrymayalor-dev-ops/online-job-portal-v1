d17d54a9ab91e33ccafb9ecbdc9b0e69762549f6
jerry-may-alor <81592848+jerry-may-alor@users.noreply.github.com>
2022-03-08 18:33:17 +0800

    03082022

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
-    <h1>Test</h1>
+    <center>
+        <h1>index</h1>
+    </center>
+
+    
 </body>
 </html>
