681d34157045e2f4fe0c61e5151acbaf5736d2e9
jerry-may-alor <81592848+jerry-may-alor@users.noreply.github.com>
2022-03-20 09:28:02 +0800

    updated 03/20/22 9:27AM

--- index.php
+++ index.php
@@ -18,14 +18,27 @@
 			<div class="container-fluid">
 				<a class="navbar-brand font-weight-bold" href="index.php">Online Municipal Job Opportunities</a> <button class="navbar-toggler" data-target="#navbarResponsive" data-toggle="collapse" type="button"><span class="navbar-toggler-icon"></span></button>
 				<div class="collapse navbar-collapse" id="navbarResponsive">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item px-3">
-							<a class="nav-link text-dark font-weight-bold" href="index.php" >Home</a>
+					<ul class="navbar-nav ml-auto text-uppercase">
+						<li class="nav-item px-1">
+							<a class="nav-link text-dark font-weight-bold" href="#" >Home</a>
 						</li>
-						<li class="nav-item px-3">
+						<li class="nav-item px-1">
+							<a class="nav-link text-dark font-weight-bold" href="#" >About Us</a>
+						</li>
+						<li class="nav-item px-1">
+							<a class="nav-link text-dark font-weight-bold" href="#" >Jobs</a>
+						</li>
+						<li class="nav-item px-1">
+							<a class="nav-link text-dark font-weight-bold" href="#" >Companies</a>
+						</li>
+						<li class="nav-item px-1">
+							<a class="nav-link text-dark font-weight-bold" href="#" >Contact us</a>
+						</li>
+
+						<li class="nav-item px-1">
 							<a class="nav-link text-dark font-weight-bold btn " href="#" data-toggle="modal" data-target="#signinModal">Sign In</a>
 						</li>
-						<li class="nav-item px-3">
+						<li class="nav-item px-1">
 							<a class="nav-link btn btn-danger text-white text-uppercase" href="#" data-toggle="modal" data-target="#signupModal">post job now!</a>
 						</li>
 						</li>
